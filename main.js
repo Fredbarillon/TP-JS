62257ae862338e1e1ccc0b615fba0e07c111044e
Fred <barillondev@gmail.com>
2025-04-22 16:49:28 +0200

    stuck at chooseRandomWord

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const BASE_URL = "https://trouve-mot.fr/api/";
 // const difficulty = "normal";
 const currentWordList = [];
 let choosenWord = null;
+const wordTyped = [];
 const difficulty = [5,10];
+let tries = 5; 
+
 /**
  * adjust to increase or decrease difficulty
  * @param {Number} length 
@@ -14,46 +17,53 @@ async function getWordList(length,number) {
     const data = await response.json();
     
     return data.map(word => word.name);
-  }
-
-//   init function
-const startGame = async () => {
-    document.querySelector("#start").addEventListener("click", async () => {
-        const worldList = await getWordList(difficulty[0],difficulty[1]);
-        currentWordList.push(...worldList);
-        console.log(currentWordList);
-        selectRandomWord();
-        detectKey();
-    });    
 }
-startGame();
 
-// select random word
 const selectRandomWord = () => {
     const i = Math.floor(Math.random() * currentWordList.length);
     choosenWord = currentWordList[i];
-    console.log("selected word:", choosenWord);   
     return choosenWord;
 }
 
+const storeWordList = async () => {
+    const worldList = await getWordList(difficulty[0],difficulty[1]);
+    currentWordList.push(...worldList);
+    return currentWordList;
+}
+
+const startGame = async () => {
+    document.querySelector("#start").addEventListener("click", async (event) => {
+        event.preventDefault();
+        currentWordList.length = 0; 
+        wordTyped.length = 0;
+        choosenWord = null;
+        tries = 5;   
+        
+        await storeWordList();
+        selectRandomWord();
+        console.log("selected word:", choosenWord);   
+        console.log(currentWordList);
+    });    
+}
+
 // detecting keyboard
-const wordTyped = [];
 const detectKey = () => {
     document.addEventListener("keydown", (event) => {
-        
+
+        if (tries === 0) return console.log("Game over. Le mot était :", choosenWord); 
+
         if (event.key.match(/^[A-Za-z]$/)) {
-            console.log(event.key+" key pressed");
-            if (wordTyped.length < 5) {
+            console.log(event.key + " key pressed");
+            if (wordTyped.length < difficulty[0]) {
                 const letter = event.key.toLowerCase();
                 wordTyped.push(letter);
-                console.log(wordString);
+                console.log(wordTyped);
             }
         }
 
-       if (event.key === "Enter") {
+        if (event.key === "Enter") {
             console.log("Enter key pressed");
-           
-            // checkWord();
+            checkWord();
         }   
     });
 }
@@ -61,32 +71,23 @@ const detectKey = () => {
 // check if the word is correct
 const checkWord = () => {
     if (wordTyped.length === difficulty[0]) {
-        const wordString = wordTyped.toString().replace(/,/g, "");  
+        const wordString = wordTyped.toString().replace(/,/g, "");
         console.log("word typed:", wordString);
+
         if (wordString === choosenWord) {
-            console.log("correct word");
-            // display success message
-        }else {
-            console.log("incorrect word");
-            // display error message
+            console.log(" gg! le mot est: ", choosenWord);
+        } else {
+            tries--; 
+            console.log(" non! essais restants :", tries);
+            if (tries === 0) {
+                console.log(" game over. le mot est :", choosenWord);
+            }
         }
-    }else {
-        console.log("word not complete");
+
+        wordTyped.length = 0; 
+    } else {
+        console.log(" Mot incomplet");
     }
 }
-
-
-
-
-
-
-
-
-// const adjsutDifficulty = () => {
-//   const easy = document.querySelector("#easy");
-//   const medium = document.querySelector("#normal"); 
-//   const hard = document.querySelector("#hard");
- 
-// };
-
-  
+startGame();
+detectKey();
